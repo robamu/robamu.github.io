4ff311b5dcfdbd49d5c6f74d4a2812b6b477a264
Robin Mueller <robin.mueller.m@pm.me>
2026-09-15 15:49:36 +0200

    update book

--- static/sat-rs/book/searcher-09f2665d.js
+++ static/sat-rs/book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0c17edf6.js',
+                path_to_root + 'searchindex-27f0876d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
